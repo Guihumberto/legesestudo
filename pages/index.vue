652cf8a7804fbf528fe92bafc42605e21c76c643
Joao Humberto <juninho.joao@hotmail.com>
2022-02-19 11:35:47 -0300

    favoritar no inicio e melhora na tela de selecao de leis

--- pages/index.vue
+++ pages/index.vue
@@ -14,13 +14,12 @@
           label="Buscar.."
           v-model="findLaw"
         ></v-text-field>
-  
-      <v-card class="mx-2 pb-2" flat color="cyan">
+      <v-card  class="mx-2 pb-2" flat color="cyan">
         <v-card-text v-if="!findLaw.length">
           <v-row>
             <v-col cols="12" sm="6" md="6" lg="4" v-for="law in listLaws" :key="law.title">
               <v-hover v-slot="{ hover }">
-                <v-card height="250" hover
+                <v-card v-show="false" height="250" hover
                     :title="law.name"
                     :to="{
                     name: 'leges',
@@ -34,11 +33,11 @@
                       </v-col>
                       <v-col cols="2">
                         <v-btn icon @click="toggleFavorite" title="favoritar">
-                        <div v-if="$auth.loggedIn">
-                          <v-icon v-if="law.favSelect" color="yellow darken-3">mdi-star</v-icon>
-                          <v-icon v-else color="grey lighten-1">mdi-star-outline</v-icon>
-                        </div>   
-                      </v-btn>
+                          <div v-if="$auth.loggedIn">
+                            <v-icon v-if="law.favSelect" color="yellow darken-3">mdi-star</v-icon>
+                            <v-icon v-else color="grey lighten-1">mdi-star-outline</v-icon>
+                          </div>   
+                        </v-btn>
                       </v-col>
                     </v-row>      
                   </v-card-title>
@@ -49,6 +48,61 @@
                     <v-chip-group column>
                       <v-chip v-for="subject in law.subjects" :key="subject.name" small class="mr-1">{{subject.name}}</v-chip>
                     </v-chip-group>
+                  </v-card-text>
+                </v-card>
+              </v-hover>
+
+        
+              <v-hover v-slot="{ hover }">
+                <v-card
+                  class="mx-auto"
+                  color="grey lighten-4"
+                  max-width="600"
+                  height="380"
+                >
+                 <nuxt-link :to="{
+                    name: 'leges',
+                    params:{leges: law.name},
+                    query:{id:law.id}  
+                  }">
+                    <v-img
+                      :aspect-ratio="16/9"
+                      src="https://cdn.pixabay.com/photo/2017/03/08/14/20/flat-2126884__340.png"
+                      contain>
+                  
+                    <v-expand-transition>
+                      <div
+                        v-if="hover"
+                        class="d-flex transition-fast-in-fast-out orange darken-2 v-card--reveal text-h2 white--text"
+                        style="height: 100%;"
+                      >
+                        {{law.law}}
+                      </div>
+                    </v-expand-transition>
+                  </v-img>
+                  </nuxt-link>
+                  <v-card-text
+                    class="pt-6"
+                    style="position: relative;"
+                  >
+                    <v-btn fab absolute right top @click="toggleFavorite(law)" title="favoritar">
+                          <div v-if="$auth.loggedIn">
+                            <v-icon v-if="law.favSelect" color="yellow darken-3">mdi-star</v-icon>
+                            <v-icon v-else color="gray">mdi-star-outline</v-icon>
+                          </div>
+                          <div v-else>
+                            <v-icon color="gray">mdi-star-outline</v-icon>
+                          </div>   
+                        </v-btn>
+                    <div class="font-weight-light grey--text text-h6 mb-2">
+                      {{law.nro_law}}
+                    </div>
+                    <h3 class="text-h6 font-weight-light orange--text mb-2">
+                      {{law.name | truncate(30)}}
+                    </h3>
+                    <div class="font-weight-light text-h7 mb-2 text-justify">
+                      {{law.description | truncate(80)}}
+                    </div>
                   </v-card-text>
                 </v-card>
               </v-hover>
@@ -87,7 +141,8 @@ export default {
   data(){
     return{
       findLaw: "",
-      favLaw: false
+      favLaw: false,
+      favoriteLaw: false,
     }
   },
 
@@ -127,12 +182,66 @@ export default {
           })
       }
       return favorite
-    }
+    },
   },
 
   methods:{
-    toggleFavorite(){
-      this.favLaw = !this.favLaw
+    lawIsLiked(index){
+      let liked = false
+      if(this.favoriteLaws){
+        const id = index
+        liked = this.favoriteLaws.some((fav) => fav.id == id )
+      }
+      this.favoriteLaw = liked
+      return liked
+    },
+    toggleFavorite(law){
+      if(!this.$auth.loggedIn){
+        return this.$store.dispatch("snackbars/setSnackbars", {text:'Faça login ou crie uma conta para adicionar uma lei ao seus favoritos', color:'error', timeout:'3000'})
+      }
+      this.lawIsLiked(law.id)
+      this.favoriteLaw = !this.favoriteLaw
+      if(this.favoriteLaw){
+        this.favoritar(law)
+      }else{
+        this.unFav(law.id)
+      }
+    },
+    favoritar(law){
+     this.$store.commit("user/addLawFav", law)
+      let userFav = this.$store.getters['user/favoritesGQL']
+
+      const variables = {
+        idUser: this.$auth.user.id,
+        favoritesLaw:userFav
+      }
+
+        this.$apollo.mutate({
+          mutation:require("../graphql/updateLikes.gql"),
+          variables:variables
+        })
+
+        this.$store.dispatch("snackbars/setSnackbars", {text:'Você adicionou uma lei ao seus favoritos', color:'success'})
+    },
+    unFav(index){
+      this.$store.commit("user/removeLawFav", index)
+
+      let userFav = this.$store.getters['user/favoritesGQL']
+
+      const variables = {
+        idUser: this.$auth.user.id,
+        favoritesLaw:userFav
+      }
+
+        this.$apollo.mutate({
+          mutation:require("../graphql/updateLikes.gql"),
+          variables:variables
+        })
+
+        this.$store.dispatch("snackbars/setSnackbars", {text:'Você Removeu uma lei dos seus favoritos', color:'error'})
+    },
+    error(){
+      this.$store.dispatch("snackbars/setSnackbars", {text:'Faça login ou crie uma conta para adicionar uma lei ao seus favoritos', color:'error', timeout:'3000'})
     }
   },
 
@@ -144,3 +253,13 @@ export default {
   }
 }
 </script>
+
+<style>
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+}
+</style>
