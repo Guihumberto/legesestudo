7701cfab35444e19ad20cc2df0a9b6296994ce83
Joao Humberto <juninho.joao@hotmail.com>
2022-02-23 09:38:00 -0300

    link

--- layouts/default.vue
+++ layouts/default.vue
@@ -65,6 +65,16 @@
           </v-list-item-title>
         </v-list-item-content>
        </v-list-item>
+       <v-list-item to="/user" v-if="$auth.loggedIn">
+         <v-list-item-icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title>
+            Dispositivos
+          </v-list-item-title>
+        </v-list-item-content>
+       </v-list-item>
      </v-list>
      </v-list-item-group>
      <!-- <template>
